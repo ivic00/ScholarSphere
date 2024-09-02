03447bbbf8b296d31c65c2f2e0b36fd5583e36e2
ivic00 <102467664+ivic00@users.noreply.github.com>
2024-09-03 00:02:36 +0200

    final commit

--- api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c371d56a4da735fac2538ee0a0bffe3e706b83c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5284ccabb6f095e6ce07d3bbdc37a22b646f3b10")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
